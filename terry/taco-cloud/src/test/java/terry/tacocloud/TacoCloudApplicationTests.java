12195f9c379658736df7fd9667a3a774747d298e
Terry <hvho1119@gmail.com>
2021-04-07 21:51:38 +0900

    (WIP) week 1 : chapter 1 - 스프링 시작하기

---
+++ terry/taco-cloud/src/test/java/terry/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package terry.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
