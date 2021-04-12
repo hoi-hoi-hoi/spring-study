c5cd7fd1fdbad91332e1aebc08eda15f3138a042
zziri <jihzang@naver.com>
2021-04-12 21:48:46 +0900

    add: model: Order, Taco, Ingredient
    주문, 타코, 재료 모델 추가

---
+++ jihoon/project/taco-cloud/src/main/java/com/zziri/tacocloud/Order.java
@@ -0,0 +1,38 @@
+package com.zziri.tacocloud;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+public class Order {
+    @NotBlank(message = "Name is required")
+    private String deliveryName;
+
+    @NotBlank(message = "Street is required")
+    private String deliveryStreet;
+
+    @NotBlank(message = "City is required")
+    private String deliveryCity;
+
+    @NotBlank(message = "State is required")
+    private String deliveryState;
+
+    @NotBlank(message = "Zip code is required")
+    private String deliveryZip;
+
+    @CreditCardNumber(message = "Not a valid credit card number")
+    private String ccNumber;
+
+    @Pattern(regexp = "^(0\\d|1\\d)(\\/)(\\d\\d)$",
+            message = "Must be formatted MM/YY")
+    private String ccExpiration;
+
+    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
+    private String ccCVV;
+}
